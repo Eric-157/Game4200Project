f5412ba3dbfbf243242d6192774f3d40760f9123
Eric Edwards <59770361+Eric-157@users.noreply.github.com>
2025-10-08 11:34:46 -0400

    scriptable object
    forgot to update changes

--- project_chef/Assets/Scripts/BasicBlock.cs
+++ project_chef/Assets/Scripts/BasicBlock.cs
@@ -11,13 +11,11 @@ public class BasicBlock : ScriptableObject
     [Header("Stats")]
     public string enemyName;
     public int health = 100;
-    public int attack = 10;
+    public int dmg = 10;
     public int defense = 5;
     public float moveSpeed = 2.0f;
     public float attackSpeed = 1.0f;
-    public int experienceValue = 50;
 
     [Header("Other")]
-    public bool isBoss = false;
     public AudioClip deathSound;
 }
