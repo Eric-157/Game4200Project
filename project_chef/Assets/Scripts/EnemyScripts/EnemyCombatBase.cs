9144aa3eaab6a9fb24ebeecc60a66f60c40ce7e2
Eric Edwards <59770361+Eric-157@users.noreply.github.com>
2025-10-29 12:32:49 -0400

    Delete project_chef directory

--- project_chef/Assets/Scripts/EnemyScripts/EnemyCombatBase.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class EnemyCombatBase : ScriptableObject
-{
-    [Tooltip("Time between attacks in seconds.")]
-    public float cooldown = 1f;
-
-    public abstract void ExecuteAttack(Enemy enemy);
-}
